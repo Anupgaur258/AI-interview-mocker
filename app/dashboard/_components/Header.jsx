e067fe09e503fa39bd33a4cfb51d1411413298fa
Anup Kumar <anup1915683@gmail.com>
2025-08-18 17:45:52 +0530

    Update Header.jsx unfixed header bcz of overlapping

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
   ]
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-20 bg-white dark:bg-gray-900 shadow-md border-b border-green-500/20">
+    <header className="top-0 left-0 right-0 z-20 bg-white dark:bg-gray-900 shadow-md border-b border-green-500/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 flex items-center justify-between font-sans">
         <Link href="/dashboard" className="flex items-center gap-2">
           <Image
